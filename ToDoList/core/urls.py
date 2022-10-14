ef809b91f8350a8b814f4ab3481e2f12bfd684d0
ruzicka <tartanec@gmail.com>
2022-10-14 08:28:31 +0200

    added django-debug-toolbar library

--- ToDoList/core/urls.py
+++ ToDoList/core/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path("api/", include("api.urls")),
 ]
 
-
+if settings.DEBUG_TOOLBAR:
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')),)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
